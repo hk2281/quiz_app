c06cf869e75e09fa5903890e726c7d0eefca6ad2
strapkobrest <strapkobrest@gmail.com>
2023-05-15 12:07:52 +0000

    - init new branch - remove old recycleview adapter emplementation

--- app/src/main/java/com/example/myapplication/ScoreActivity.java
+++ app/src/main/java/com/example/myapplication/ScoreActivity.java
@@ -17,10 +17,7 @@ public class ScoreActivity extends AppCompatActivity {
 
 
         setInitialData();
-        Log.d("fa","Score act on create");
-        RecyclerView recyclerView = findViewById(R.id.listInActivityScore);
-        StateAdapter adapter = new StateAdapter(this, states);
-        recyclerView.setAdapter(adapter);
+
 
 
     }
